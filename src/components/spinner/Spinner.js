5b53e8111aaf297e438a7730b9a92ca5514cc62b
boomer77777 <max.yanchuk36@gmail.com>
2023-06-15 19:00:32 +0300

    first commit

---
+++ src/components/spinner/Spinner.js
@@ -0,0 +1,12 @@
+import './spinner.scss'
+
+const Spinner = () => {
+    
+    return (
+        <div className="loadingio-spinner-spinner-01h9n7d7ghuw"><div className="ldio-wpftaml53dj">
+        <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+        </div></div>
+    )
+}
+
+export default Spinner
