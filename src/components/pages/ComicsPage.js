64fcefcf4b2036a14c38803fa27a1fa3d0430804
boomer77777 <max.yanchuk36@gmail.com>
2023-07-17 16:34:42 +0300

    added single layout for comics and character page

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,10 +1,18 @@
 import AppBanner from "../appBanner/AppBanner";
+import { Helmet } from "react-helmet";
 import ComicsList from "../comicsList/ComicsList"
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 const ComicsPage = () => {
     return (
         <>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Page with list of our comics"
+                />
+                <title>Comcics page</title>
+            </Helmet>
             <AppBanner/>
             <ErrorBoundary>
                 <ComicsList/>
